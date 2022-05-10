12660feaa58e3f79372b5b09d0c72b9a31b0e58b
Santosh Kumar <40932902+withusanty@users.noreply.github.com>
2022-05-10 13:23:13 +0530

    Add files via upload

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 from dash import html, dcc
 
 
-def input_field(header, id, placeholder, options, size=2, offset=0, text_align='center',value = 0):
+def input_field(header, id, placeholder, options, size=2, offset=0, text_align='center',value = None):
     input = dbc.Col([html.Header(header),
                      dcc.Dropdown(id=id, multi=False, value=value, placeholder=placeholder, optionHeight=20,
                                   options=options,
@@ -14,19 +14,14 @@ def input_field(header, id, placeholder, options, size=2, offset=0, text_align='
     return input
 
 
-def time_select(hour_id, min_id,value=0,value2=0):
+def time_select(hour_id, min_id,value=None,value2=None):
     time = dbc.Row([
         dbc.Col([
-            dcc.Dropdown([x for x in range(0, 24)], placeholder="Hour", id=hour_id, style={'text-align': 'start'},
-                         className='text-black',value=value),
-        ]),
-        dbc.Col([
-            dcc.Dropdown([x for x in range(0, 60)], placeholder="Minute", id=min_id, style={'text-align': 'start'},
-                         className='text-black',value=value2),
+            dcc.Input(id=hour_id, type='number', placeholder='Hour', min=0, max=23,value=value,style={'width':80}),
+            dcc.Input(id=min_id, type='number', placeholder='Minute', min=0, max=59,value=value2,style={'width':80}),
         ])
     ])
     return time
-
 
 def airlines(x=0):
     """
@@ -63,11 +58,24 @@ def destination(x=0):
     return dest_val
 
 
-
-
 def date_month_wk(x):
     import datetime
     input = x
     format = '%Y-%m-%d'
     datetime = datetime.datetime.strptime(input, format)
-    return [datetime.date().day, datetime.date().month, datetime.date().isocalendar().week]
+    return [ datetime.date().month, datetime.date().day, datetime.date().isocalendar().week]
+
+
+def flight_class(x):
+    classs =  ['Business','First','Economy']
+    if x == 'Business': return 2
+    elif x == 'First': return 1
+    else : return 0
+
+def get_arrival_time(dep_h,dep_m,trav_h,trav_m):
+    from datetime import datetime, timedelta
+    datetime_original = datetime(2022, 6, 6, dep_h, dep_m)
+    time_delta = timedelta(hours=trav_h, minutes=trav_m)
+    datetime_new = datetime_original + time_delta
+    arriv_hm = [datetime_new.hour,datetime_new.minute]
+    return arriv_hm
